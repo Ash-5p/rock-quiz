a4167b814c5eb627df58bb4b9605ee179994f067
Ashley Preece <ashleyjph@gmail.com>
2024-08-20 17:35:36 +0000

    Add question and answer html

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+//Global Variables
+let homeScreen = document.getElementsByClassName('home')[0];
+let howTo = document.getElementsByClassName('how-to')[0];
+let gameArea = document.getElementsByClassName('game-area')[0];
+
+startGame()
+
+shuffleQuestions()
+
+checkAnswer()
+
+isCorrect()
+
+isIncorrect()
+
+timer()
+
+scoreTracker()
