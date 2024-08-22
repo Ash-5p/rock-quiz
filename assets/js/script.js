36a23f94093af64b9296ed002b3666b721b3f20a
Ashley Preece <ashleyjph@gmail.com>
2024-08-22 17:22:47 +0000

    Add event listeners to menu buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,24 @@
 let homeScreen = document.getElementsByClassName('home')[0];
 let howTo = document.getElementsByClassName('how-to')[0];
 let gameArea = document.getElementsByClassName('game-area')[0];
+let answerArea = document.getElementsByClassName('answers')[0];
+let questionArea = document.getElementsByClassName('question')[0];
+let menuButtons = document.getElementsByClassName('menu-btn');
 
-startGame()
+//Get menu button elements and add event listeners
+for (let menuButton of menuButtons) {
+    menuButton.addEventListener('click', function () {
+        menuControls(menuButton);
+    });
+}
+
+function menuControls(button) {
+    // Check the id of the button that was clicked
+    if(button.id === "htp-btn") {
+        homeScreen.classList.add('hide');
+        howTo.classList.remove('hide');
+    }
+}
 
 shuffleQuestions()
 
